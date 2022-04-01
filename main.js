b9c185be25677da273f268ac37a97a9d4f6241c3
Mohammad Yusuf Beg <82448349+Yusuf-max552@users.noreply.github.com>
2022-04-01 11:45:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 function bed(){
-    window.location = "tv.html";
+    window.location = "bed.html";
 }
 function tv(){
-    window.location = "bed.html";
+    window.location = "tv.html";
+}
+function laptop(){
+    window.location = "laptop.html";
+}
+function bottles(){
+    window.location = "bottles.html";
+}
+function fruits(){
+    window.location = "fruits.html";
 }
